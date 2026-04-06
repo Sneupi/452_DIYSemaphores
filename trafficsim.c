cc6a5d1966373d47c5ebe3dcd4322b8a280fbedd
Gabe <venegasg1@arizona.edu>
2026-04-06 15:50:13 -0700

    p much done

--- trafficsim.c
+++ trafficsim.c
@@ -17,6 +17,7 @@
 #include <sys/mman.h>
 #include <string.h>
 #include <sys/syscall.h>
+#include <time.h>
 
 // Using void* to avoid 
 // duplication clutter
@@ -30,6 +31,7 @@ struct csc452_sem {
 
 struct sim_data {
     int counter;
+    int start_time;
 
     int e;
     int w;
@@ -69,11 +71,11 @@ void producer(struct sim_data *s, int isWest) {
         queue[*idx] = item;
         (*idx)++;
         s->counter++;
-        printf("Car %d coming from the %c direction arrived in the queue at time %d.\n", item, dir, -1);
+        printf("Car %d coming from the %c direction arrived in the queue at time %d.\n", item, dir, (int)time(NULL)-(s->start_time));
         
         // if first arrival, honk
         if ((*idx)-1 == 0 && *idx_opp == 0) {
-            printf("Car %d coming from the %c direction, blew their horn at time %d.\n", item, dir, -1);
+            printf("Car %d coming from the %c direction, blew their horn at time %d.\n", item, dir, (int)time(NULL)-(s->start_time));
             up(&(s->honk));
         }
 
@@ -90,37 +92,57 @@ void producer(struct sim_data *s, int isWest) {
 void consumer(struct sim_data *s) {
 
     // init sleeping, await first honk
+    int isWest = 1;
     down(&(s->honk));
 
     // process each item
     while (1) {
 
-        // sleep until woken
+        // if no activity, sleep
         down(&(s->mutex));
         if (s->e == 0 && s->w == 0) {
             printf("The flagperson is now asleep.\n");
             up(&(s->mutex));
             down(&(s->honk));
             printf("The flagperson is now awake.\n");
-        }
+        } 
+        // if opposite exceeds 8 pileup, flip
+        else if (
+            (isWest && !s->w && s->e) ||
+            (!isWest && !s->e && s->w) ||
+            (isWest && s->w < 8 && s->e >= 8) ||
+            (!isWest && s->e < 8 && s->w >= 8)
+        ) {
+            isWest = !isWest;
+            up(&(s->mutex));
+        } 
+        // (check normal exit)
         else {
             up(&(s->mutex));
         }
 
-        // dequeue
-        down(&(s->full_e));
+        // generalized vars
+        int *queue = (isWest) ? s->queue_w : s->queue_e;
+        int *idx = (isWest) ? &(s->w) : &(s->e);
+        int *idx_opp = (isWest) ? &(s->e) : &(s->w);
+        const char dir = (isWest) ? 'W' : 'E';
+        struct csc452_sem *empty = (isWest) ? &(s->empty_w) : &(s->empty_e);
+        struct csc452_sem *full = (isWest) ? &(s->full_w) : &(s->full_e);
+
+        // dequeue an item
+        down(full);
         down(&(s->mutex));
-        int item = s->queue_e[0]; 
-        for (int i = 0; i < s->e - 1; i++) {
-            s->queue_e[i] = s->queue_e[i + 1];
+        int item = queue[0]; 
+        for (int i = 0; i < (*idx) - 1; i++) {
+            queue[i] = queue[i + 1];
         }
-        s->e--;
+        (*idx)--;
         up(&(s->mutex));
-        up(&(s->empty_e));
+        up(empty);
         
         // process the item
         sleep(1);
-        printf("Car %d coming from the %c direction left the construction zone at time %d.\n", item, -1, -1);
+        printf("Car %d coming from the %c direction left the construction zone at time %d.\n", item, dir, (int)time(NULL)-(s->start_time));
     }
 }
 
@@ -158,21 +180,32 @@ int main() {
     seminit(&(data->mutex), 1);
     seminit(&(data->honk), 0);
 
-    // Begin main program simulation
-    
-    pid_t pid = fork();
+    data->start_time = (int)time(NULL);
 
-    if (pid < 0) {
-        perror("Fork failed");
+    // spawn traffic 1 (child)
+    pid_t pid_e = fork();
+    if (pid_e < 0) {
+        perror("Fork 1 failed");
         return 1;
+    } else if (pid_e == 0) {
+        producer(data, 1);
+        exit(0);
     }
 
-    if (pid == 0) {
-        consumer(data);
-    } else {
+    // spawn traffic 2 (child)
+    pid_t pid_w = fork();
+    if (pid_w < 0) {
+        perror("Fork 2 failed");
+        return 1;
+    } else if (pid_w == 0) {
         producer(data, 0);
-        wait(NULL); // Keep parent alive
-    }
+        exit(0);
+    } 
+    
+    // run flag person (parent)
+    consumer(data);
+    wait(NULL);
+    wait(NULL);
 
     return 0;
 }
